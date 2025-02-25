4c0c5939829e53b02cf779f342b68e57877fffd4
IOSEdwin <eramirezn23@icloud.com>
2025-02-25 11:13:00 +0100

    asignamos botones interactivos para altura y peso

--- app/src/main/java/com/example/imccalculator/MainActivity.kt
+++ app/src/main/java/com/example/imccalculator/MainActivity.kt
@@ -2,20 +2,28 @@ package com.example.imccalculator
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.slider.Slider
+import java.util.Locale
 import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var heightEditText: EditText
-    lateinit var weightEditText: EditText
-    lateinit var calculateButton : Button
-    lateinit var resultadoIMC : TextView
+    lateinit var heightTextView: TextView
+    lateinit var heightSlider:Slider
+
+    lateinit var weightTextView: TextView
+    lateinit var weightButtonMinus: Button
+    lateinit var weightButtonPlus: Button
+
+    lateinit var calculateButton: Button
+    lateinit var resultadoIMC: TextView
+
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,20 +35,44 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        heightEditText = findViewById(R.id.alturaText)
-        weightEditText = findViewById(R.id.pesoText)
+
+        heightTextView =findViewById(R.id.heightTextView)
+        heightSlider =findViewById(R.id.slider)
+
+        weightTextView =findViewById(R.id.weightTextView)
+        weightButtonPlus =findViewById(R.id.weightPlusButton)
+        weightButtonMinus =findViewById(R.id.weightMinusButton)
+
         calculateButton = findViewById(R.id.calculateButton)
         resultadoIMC = findViewById(R.id.resultadoIMC)
 
 
+        var height = 170.0f
+        var weight = 70.0f
 
-        calculateButton.setOnClickListener {
-            var altura= heightEditText.text.toString().toDouble()
-            var peso= heightEditText.text.toString().toDouble()
 
-//            val result = weight / (height / 100).pow(2)
-            val resultado= peso/(altura/100).pow(2)
-            resultadoIMC.text="$resultado"
+            heightSlider.addOnChangeListener { slider, value, fromUser ->
+                height = value
+                heightTextView.text = "${height.toInt()}"
+            } // revisar la documentaciion para ver donde sale este fargmnto de codigo del Slider
+
+            weightButtonPlus.setOnClickListener(){
+                weight++
+                weightTextView.text="${weight.toInt()}"
+            }
+            weightButtonMinus.setOnClickListener(){
+            weight--
+            weightTextView.text="${weight.toInt()}"
+            }
+
+            calculateButton.setOnClickListener {
+
+            val resultado = weight / (height / 100).pow(2)
+                resultadoIMC.text = String.format(Locale.getDefault(), "%.2f", resultado)
         }
+
+
+
     }
 }
+
